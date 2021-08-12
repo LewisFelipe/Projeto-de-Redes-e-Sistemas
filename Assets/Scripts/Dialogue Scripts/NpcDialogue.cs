b2750e60398797defd43cf0ea5c2ee6161a8d41e
LucasMartineli <82618558+LucasMartineli@users.noreply.github.com>
2021-08-12 17:18:22 -0300

    resolução de bugs

---
+++ Assets/Scripts/Dialogue Scripts/NpcDialogue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NpcDialogue : MonoBehaviour
+{
+
+    public string dialogueText;
+    public Image dialoguePanel;
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter()
+    {
+        Debug.Log("Funcionando");
+    }
+}
