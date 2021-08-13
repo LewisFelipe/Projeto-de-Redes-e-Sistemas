66b6c1d18fe32dc458b8f90ce452f218bd88c148
LucasMartineli <82618558+LucasMartineli@users.noreply.github.com>
2021-08-13 01:38:16 -0300

    Animação de ataque espada

---
+++ Assets/Scripts/Skills & Attacks Scripts/BasicAttack.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAttack : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rb;
+    private bool isBlocking = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        BasicHit();
+    }
+
+    private void BasicHit()
+    {
+        //Basic Sword
+        if(WeaponID.Instance.weaponActive == true && WeaponID.Instance.ID == "BasicSword")
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                animator.SetTrigger("Attacking");
+            }
+            else
+            {
+                animator.ResetTrigger("Attacking");
+            }
+
+            if(Input.GetMouseButtonDown(1))
+            {
+                animator.SetTrigger("Blocking");
+                animator.SetBool("FinishAnimation", false);
+                rb.constraints = RigidbodyConstraints.FreezePosition;
+            }
+            else if(Input.GetMouseButtonUp(1))
+            {
+                animator.ResetTrigger("Blocking");
+                animator.SetBool("FinishAnimation", true);
+                rb.constraints = RigidbodyConstraints.None;
+                rb.constraints = RigidbodyConstraints.FreezeRotation;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Skills & Attacks Scripts/WeaponID.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponID : MonoBehaviour
+{
+    public static WeaponID Instance;
+    public string weaponName;
+    [HideInInspector] public string ID;
+    [HideInInspector] public bool weaponActive;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Update()
+    {
+        if(gameObject.activeSelf)
+        {
+            ID = weaponName;
+            weaponActive = true;
+        }
+        else
+        {
+            weaponActive = false;
+        }
+    }
+}
