865a4f54b29f8a30c942d7f314f6acbb6e5db0b9
fabioyonamine <fabio.m.yonamine@gmail.com>
2021-08-14 21:26:44 -0300

    Animação saindo dos limites
    Alguns scripts e outras fitas

---
+++ Assets/Scripts/outOfTheBonds.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class outOfTheBonds : MonoBehaviour
+{
+    private bool estaDentro;
+
+    public GameObject luzPlayer;
+    [SerializeField] private Animator myAnimationController;
+    
+    void OnTriggerEnter(){
+        estaDentro = true;
+    }
+
+    void OnTriggerExit(){
+        estaDentro = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(estaDentro == true){
+            luzPlayer.SetActive(true);
+            myAnimationController.SetBool("PlayerOut", false);
+            
+        } else {
+            luzPlayer.SetActive(false);
+            myAnimationController.SetBool("PlayerOut", true);
+        }
+    }
+}
