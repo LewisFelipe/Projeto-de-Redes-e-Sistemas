0896da18c5546c03c4a33e019c08bce6af2b13d7
LucasMartineli <82618558+LucasMartineli@users.noreply.github.com>
2021-08-23 22:38:44 -0300

    precaução

---
+++ Assets/Scripts/AI Scripts/BossSkills.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSkills : MonoBehaviour
+{
+    private Animator animator;
+    private BossHealth bossStats;
+    private bool isHealing = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        bossStats = GetComponent<BossHealth>();
+    }
+
+    
+    void Update()
+    {
+        CheckAnimations();
+    }
+
+    private void CheckAnimations()
+    {
+        if(animator.GetCurrentAnimatorStateInfo(0).IsTag("EnemyHeal") && isHealing == false)
+        {
+            bossStats.bossHealth += 100;
+            isHealing = true;
+            bossStats.ChangeHealthBar();
+        }
+    }
+}
