a83bad0c90143b8670da6e5c147ad6c8d2a567a8
LucasMartineli <82618558+LucasMartineli@users.noreply.github.com>
2021-09-01 12:08:24 -0300

    boss

--- Assets/Scripts/AI Scripts/BossSkills.cs
+++ Assets/Scripts/AI Scripts/BossSkills.cs
@@ -7,6 +7,7 @@ public class BossSkills : MonoBehaviour
     private Animator animator;
     private BossHealth bossStats;
     private bool isHealing = false;
+    public SphereCollider rightHand, LeftHand;
 
     void Start()
     {
@@ -28,5 +29,36 @@ public class BossSkills : MonoBehaviour
             isHealing = true;
             bossStats.ChangeHealthBar();
         }
+
+        if(animator.GetCurrentAnimatorStateInfo(0).IsTag("EnemyAttack01"))
+        {
+            isHealing = false;
+        }
+
+        if(animator.GetCurrentAnimatorStateInfo(0).IsTag("EnemyAttack02"))
+        {
+            rightHand.enabled = true;
+            LeftHand.enabled = true;
+            isHealing = false;
+        }
+        else if(animator.GetCurrentAnimatorStateInfo(0).IsTag("EnemyAttack02") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+        {
+            rightHand.enabled = false;
+            LeftHand.enabled = false;
+        }
+
+        if(animator.GetCurrentAnimatorStateInfo(0).IsTag("EnemyAttack03"))
+        {
+            rightHand.enabled = true;
+            LeftHand.enabled = true;
+            isHealing = false;
+        }
+        else if(animator.GetCurrentAnimatorStateInfo(0).IsTag("EnemyAttack03") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+        {
+            rightHand.enabled = false;
+            LeftHand.enabled = false;                
+        }
     }
+
+
 }
---
+++ Assets/Scripts/Skills & Attacks Scripts/BossHandHit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHandHit : MonoBehaviour
+{
+    PlayerHealth playerHealth;
+    private bool isTriggered = false;
+
+    private void Start()
+    {
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && !isTriggered)
+        {
+            playerHealth.health -= 15;
+            playerHealth.ChangeHealthBar();
+            isTriggered = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && !isTriggered)
+        {
+            isTriggered = false;
+        }        
+    }
+}
