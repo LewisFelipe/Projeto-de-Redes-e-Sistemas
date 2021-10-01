1074b257ad8ecfcc75d0f2f848e26039ca662b42
LewisFelipe <aluisiusfeliposu@gmail.com>
2021-09-30 22:37:38 -0300

    Programação do Menu quase pronta

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,22 +5,67 @@ using UnityEngine.UI;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
-{
+{   
     //Ainda falta salvar com player prefs
+    Resolution[] resolutions;
+    public Dropdown resolutionsDropdown;
     public GameObject optionsPanel;
+    LTRect opTionsPanelRectTransform = new LTRect();
     public Image muteMusic, muteSound;
     public Sprite musicOn, musicOff, soundOn, soundOff;
     public Slider musicSlider, soundSlider;
     public AudioSource musicEmitter, soundEmitter;
 
+    private void StartResolutions()
+    {
+        int currentResolutionIndex = 0;
+        resolutions = Screen.resolutions;
+        List<string> resolutionsList = new List<string>();
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            resolutionsList.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionsDropdown.ClearOptions();
+        resolutionsDropdown.AddOptions(resolutionsList);
+        resolutionsDropdown.value = currentResolutionIndex;
+        resolutionsDropdown.RefreshShownValue();
+    }
+
     public void OptionsButton()
     {
-        optionsPanel.SetActive(!optionsPanel.activeSelf);
+        //optionsPanel.SetActive(!optionsPanel.activeSelf);
+        LeanTween.move(optionsPanel, new Vector2(Screen.currentResolution.width / 2, Screen.currentResolution.height / 2), 0.5f);
     }
 
     public void OptionsButtonExit()
     {
-        optionsPanel.SetActive(!optionsPanel.activeSelf);
+        LeanTween.move(optionsPanel, new Vector2(Screen.currentResolution.width / 2, -Screen.currentResolution.height), 0.5f);
+    }
+    
+    public void SetResolution(int resolutionIndex)
+    {
+        Screen.SetResolution(resolutions[resolutionIndex].width, resolutions[resolutionIndex].height, Screen.fullScreen); 
+    }
+
+    public void SetFullScreen(bool isOn)
+    {
+        Screen.fullScreen = isOn;
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetVSync(bool isOn)
+    {
+        QualitySettings.vSyncCount = Convert.ToInt32(isOn);
     }
 
     private void ChangeMuteUI(AudioSource source, Image UIButton, Sprite on, Sprite off)
@@ -70,6 +115,9 @@ public class MenuManager : MonoBehaviour
 
     private void Start()
     {
-        optionsPanel.SetActive(false);
+        StartResolutions();
+
+        //opTionsPanelRectTransform = optionsPanel.GetComponent<RectTransform>();
+        //optionsPanel.SetActive(false);
     }
 }
