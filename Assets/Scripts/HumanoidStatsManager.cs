2cc5caf578c013f4d4203406cf4548b2cb21d593
LewisFelipe <aluisiusfeliposu@gmail.com>
2021-10-21 21:35:44 -0300

    God Rotation fufando
    Eu to indo fazer a tela de pause, perdão ai a demora.

--- Assets/Scripts/HumanoidStatsManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HumanoidStatsManager : MonoBehaviour
-{
-    float speed, maxhealth, health, shield;
-    
-}
